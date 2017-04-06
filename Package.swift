12b33f2416d0698180748cb949cc6cb3dc50f5c7
twof <fabiobean2@gmail.com>
2017-04-06 12:26:28 -0700

    Adding models

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "parkr-api",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,11 +1,39 @@
 import Vapor
+import VaporPostgreSQL
 
-let drop = Droplet()
+let drop = Droplet(preparations: [Acronym.self])
+
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
+}
+
+drop.get("hello") { request in
+    return "Hello, world!"
+}
+
+drop.get("version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    }else{
+        return "No db connection"
+    }
+}
+
+drop.get("model") { request in
+    let acronym = Acronym(short: "AFK", long: "Away From Keyboard")
+    
+    return try acronym.makeJSON()
+}
+
+drop.get("test") { request in
+    var acronym = Acronym(short: "AFK", long: "Away From Keyboard")
+    try acronym.save()
+    return try JSON(node: Acronym.all().makeNode())
 }
 
 drop.resource("posts", PostController())
