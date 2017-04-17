37f4b665c42d2d4c7a343ce9ef1852b53958ec34
twof <fabiobean2@gmail.com>
2017-04-17 10:37:42 -0700

    update package

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,7 +1,10 @@
+import Foundation
 import Vapor
 import VaporPostgreSQL
 import Alamofire
 import ObjectMapper
+import GeoSwift
+import MapKit
 
 let drop = Droplet(preparations: [Acronym.self, ParkingRule.self])
 
@@ -20,6 +23,12 @@ let resp = try drop.client.get("https://data.sfgov.org/resource/2ehv-6arf.json",
 let j = resp.json
 
 print((resp.json?[0])!)
+
+if let geoJSONURL = Bundle.main.url(forResource: "TimedParkingData", withExtension: "geojson"),
+    let geometries = try! Geometry.fromGeoJSON(geoJSONURL),
+    let italy = geometries[0] as? MultiPolygon {
+    
+}
 
 drop.get("hello") { request in
     
@@ -46,7 +55,7 @@ drop.get("model") { request in
 drop.get("test") { request in
     var acronym = Acronym(short: "AFK", long: "Away From Keyboard")
     try acronym.save()
-    return try JSON(node: Acronym.all().makeNode())
+    return try JSON(node: Acronym.query().all().makeNode())
 }
 
 drop.resource("posts", PostController())
