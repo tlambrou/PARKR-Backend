21d84e6b8046dc1d197aaa218284ae25e2388ab1
tlambrou <lambrou@gmail.com>
2017-04-16 21:58:09 -0700

    Package.swift merge conflict

--- Package.swift
+++ Package.swift
@@ -6,13 +6,8 @@ let package = Package(
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1),
-<<<<<<< HEAD
-        .Package(url: "https://github.com/andreacremaschi/GEOSwift", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
-=======
         .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
         .Package(url: "https://github.com/petrpavlik/GeoSwift.git", majorVersion: 1)
->>>>>>> 5aad70187a2656a14855b9fb1b7201bd4f98e033
     ],
     exclude: [
         "Config",
