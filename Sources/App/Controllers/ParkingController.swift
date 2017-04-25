a7807024c7238e36b5e7c0ec6c7139f1b7054444
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-04-25 12:38:08 -0700

    more sql

--- Sources/App/Controllers/ParkingController.swift
+++ Sources/App/Controllers/ParkingController.swift
@@ -20,31 +20,43 @@ final class ParkingController {
          */
         
         guard let ulatleft = request.parameters["ULat"]?.float, // x1
-        let ulong = request.parameters["ULong"]?.float, // y1
-        let llat = request.parameters["LLat"]?.float, // x2
-        let llong = request.parameters["LLong"]?.float else { // y2
+        let ulongleft = request.parameters["ULong"]?.float, // y1
+        let llatright = request.parameters["LLat"]?.float, // x2
+        let llongright = request.parameters["LLong"]?.float else { // y2
             throw Abort.badRequest
         }
         
-        let parkings = try Parking.query()
+        let ulatright = ulatleft + (llatright - ulatleft)
+        let ulongright = ulongleft
+        let llatleft = ulatleft
+        let llongleft = ulongleft + (llongright - ulongleft)
         
-        select *
-        from parkings
-        where
-        bounding_x1 > ulat and bounding_x1 < llat AND bounding_y1 > ulong and bounding_y1 < llong
-        bounding_x2 > ulat and bounding_x2 < llat AND bounding_y2 > ulong and bounding_y2 < llong
-        calc_bounding_x3 > ulat and calc_bounding_x3 < llat AND calc_bounding_y3 > ulong and calc_bounding_y3 < llong
-        calc_bounding_x4 > ulat and calc_bounding_x4 < llat AND calc_bounding_y4 > ulong and calc_bounding_y4 < llong
+//        let parkings = try Parking.query().filter("bounding_x1", .greaterThanOrEquals, ulatleft)
+//        .filter("bounding_x2", .lessThanOrEquals, )
         
-        select *
-        from parkings
-        where
-        ulat > bounding_x1 and llat < bounding_x1 AND ulong > bounding_y1 and llong < bounding_y1
-        ulat > bounding_x2 and llat < bounding_x2 AND ulong > bounding_y2 and llong < bounding_y2
-        ulat > calc_bounding_x3 and ulat < llat AND calc_bounding_y3 > ulong and calc_bounding_y3 < llong
-        calc_bounding_x4 > ulat and calc_bounding_x4 < llat AND calc_bounding_y4 > ulong and calc_bounding_y4 < llong
+        var parkings: [Node]
         
-        return "stuff"
+        if let mysql = drop.database?.driver as? PostgreSQLDriver {
+            let parkings = try mysql.raw("SELECT * FROM parkings WHERE (bounding_x1 BETWEEN \(ulatleft) AND \(llatright)) AND (bounding_y1 BETWEEN \(ulongleft) AND \(llongright))").nodeArray
+        }
+        
+//        select *
+//        from parkings
+//        where
+//        bounding_x1 > ulat and bounding_x1 < llat AND bounding_y1 > ulong and bounding_y1 < llong
+//        bounding_x2 > ulat and bounding_x2 < llat AND bounding_y2 > ulong and bounding_y2 < llong
+//        calc_bounding_x3 > ulat and calc_bounding_x3 < llat AND calc_bounding_y3 > ulong and calc_bounding_y3 < llong
+//        calc_bounding_x4 > ulat and calc_bounding_x4 < llat AND calc_bounding_y4 > ulong and calc_bounding_y4 < llong
+//        
+//        select *
+//        from parkings
+//        where
+//        ulat > bounding_x1 and llat < bounding_x1 AND ulong > bounding_y1 and llong < bounding_y1
+//        ulat > bounding_x2 and llat < bounding_x2 AND ulong > bounding_y2 and llong < bounding_y2
+//        ulat > calc_bounding_x3 and ulat < llat AND calc_bounding_y3 > ulong and calc_bounding_y3 < llong
+//        calc_bounding_x4 > ulat and calc_bounding_x4 < llat AND calc_bounding_y4 > ulong and calc_bounding_y4 < llong
+        
+        return parkings as! ResponseRepresentable
     }
     
     func create(request: Request) throws -> ResponseRepresentable {
