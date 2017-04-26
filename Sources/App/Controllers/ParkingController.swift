59a700b360f2b1e15cec6f33a61b6e487978248c
twof <fabiobean2@gmail.com>
2017-04-25 17:33:30 -0700

    Cleaned parking controller

--- Sources/App/Controllers/ParkingController.swift
+++ Sources/App/Controllers/ParkingController.swift
@@ -4,22 +4,16 @@ import VaporPostgreSQL
 
 final class ParkingController: ResourceRepresentable {
     
+    
     func parkingSubset(request: Request) throws -> ResponseRepresentable {
-        /*
-         API call for getting all blocks intersecting with the view's bounding rectangle.
-         http://parkr-api.herokuapp.com/parking/subset?ULat=37.766952&ULong=-122.412581&LLat=37.765095&LLong=-122.413949
-         
-         Base URL: http://parkr-api.herokuapp.com/parking/subset
-         
-         Parameters
-         ULat: Represents upper left latitude coordinate of current bounding box
-         ULong: Represents upper left longitude coordinate of current bounding box
-         LLat: Represents lower right latitude coordinate of current bounding box
-         LLong: Represents lower right longitude coordinate of current bounding box
-         */
         return "hello world"
     }
     
+    /*
+     Exists to ingest data from the dataset we're dealing with and transform it into a format that's usable in our DB
+     Once in the DB, we're free to query for a square of data that would intersect the viewfinder on the ios app, and turn things into objects which would allow us to do analysis.
+     This will eventually be used to ingest multiple data sets and normalize the data.
+     */
     func ingestion(request: Request) throws -> ResponseRepresentable {
         let resp = try drop.client.get("https://data.sfgov.org/resource/2ehv-6arf.json", headers: ["X-App-Token": "kvtD98auzsy6uHJqGIpB7u1tq"], query: [:], body: "").json!
         
@@ -37,59 +31,10 @@ final class ParkingController: ResourceRepresentable {
                 print(error)
             }
         }
-        
-        
         return try Parking.all().makeJSON()
     }
     
-    
-    func index(request: Request) throws -> ResponseRepresentable {
-        return try JSON(node: Parking.all().makeNode())
-    }
-    
-    func create(request: Request) throws -> ResponseRepresentable {
-        var parking = try request.parking()
-        try parking.save()
-        return parking
-    }
-    
-    func show(request: Request, parking: Parking) throws -> ResponseRepresentable {
-        return parking
-    }
-    
-    func update(request: Request, parking: Parking) throws -> ResponseRepresentable {
-        let new = try request.parking()
-        let parking = parking
-        
-        parking.dayRange = new.dayRange
-        parking.hourLimit = new.hourLimit
-        parking.hoursBegin = new.hoursBegin
-        parking.hoursEnd = new.hoursEnd
-        
-        return parking
-    }
-    
-    func delete(request: Request, parking: Parking) throws -> ResponseRepresentable {
-        try parking.delete()
-        return JSON([:])
-    }
-    
     func makeResource() -> Resource<Parking> {
-        return Resource(
-            index: index,
-            store: create,
-            show: show,
-            modify: update,
-            destroy: delete
-        )
-    }
-    
-    //  TODO: Make a get method route that takes in bounding rectangle coordinates and returns JSON data of all data that intersects with that view.
-}
-
-extension Request {
-    func parking() throws -> Parking {
-        guard let json = json else { throw Abort.badRequest }
-        return try Parking(node: json)
+        return Resource()
     }
 }
